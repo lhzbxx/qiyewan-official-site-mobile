5fe6f35573b6a1f8c0c8afdd758a7852800a075f
lhzbxx <lhzbxx@gmail.com>
2016-11-21 12:28:32 +0800

    Fix areas.

--- src/pages/Areas.vue
+++ src/pages/Areas.vue
@@ -2,39 +2,40 @@
   <div id="areas">
     <label class="mint-radiolist-title">当前位置</label>
     <mt-cell
-      title="标题文字"
-      value="已选地区"
-      v-for="item in payments">
+      :title="getRegion.name"
+      :label="getRegion.pName"
+      value="已选地区">
     </mt-cell>
-    <label class="mint-radiolist-title">当前位置</label>
-    <mt-radio
-      align="right"
-      v-model="payment"
-      title="全部地区"
-      :options="payments">
-    </mt-radio>
+    <label class="mint-radiolist-title">全部地区</label>
+    <mt-cell
+      :title="item.name"
+      :label="item.pName"
+      :value="isSelected(item) ? '已选地区' : ''"
+      @click.native="selectRegion(item)"
+      v-for="item in getRegions">
+    </mt-cell>
   </div>
 </template>
 
 <script>
+  import {mapGetters} from 'vuex'
+
   export default {
     data() {
-      return {
-        payment: 'WeChat',
-        payments: [
-          {
-            label: '微信支付',
-            value: 'WeChat',
-          },
-          {
-            label: '支付宝',
-            value: 'AliPay'
-          },
-          {
-            label: '银行卡',
-            value: 'Bank',
-          }
-        ]
+      return {}
+    },
+    computed: {
+      ...mapGetters([
+        'getRegions',
+        'getRegion'
+      ])
+    },
+    methods: {
+      isSelected(region) {
+        return region.code == this.getRegion.code
+      },
+      selectRegion(region) {
+        this.$store.commit('CHANGE_REGION', region)
       }
     }
   }
