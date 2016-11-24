57b4ac74eb0789841794dd582fddeff84abcb14b
lhzbxx <lhzbxx@gmail.com>
2016-11-24 17:04:18 +0800

    Fix not found page.

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,5 +1,45 @@
 <template>
   <div id="not-found">
-    <h1>404</h1>
+    <img src="../assets/logo.png">
+    <h1>哎呀，出错了</h1>
+    <mt-button type="primary"
+               size="small"
+               @click.native="jump"
+               id="button">
+      回到首页
+    </mt-button>
   </div>
 </template>
+<script>
+  export default {
+    methods: {
+      jump() {
+        this.$router.push({name: 'home'})
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #not-found {
+    margin-top: 80px;
+    text-align: center;
+  }
+
+  #not-found img {
+    width: 50%;
+    display: block;
+    margin: 0 auto;
+  }
+
+  h1 {
+    margin-top: 20px;
+    color: #aaa;
+    font-size: 22px;
+  }
+
+  #button {
+    display: block;
+    margin: 20px auto;
+  }
+</style>
