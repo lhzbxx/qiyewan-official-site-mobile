67e2d36b6fb823377eaf42a2c17c8ac21c9a0a66
lhzbxx <lhzbxx@gmail.com>
2016-12-01 11:12:02 +0800

    Fix about-us page.

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -1,9 +1,50 @@
 <template>
   <div id="about-us">
     <lh-page-header title="关于我们"></lh-page-header>
+    <img :src="'wechat_about-us.png' | cdn-filter" style="width: 100%;">
+    <div id="content">
+      <h3>企业湾</h3>
+      <p>
+        企业湾（www.qiyewan.com）是国内领先的专注于中小企业服务的一站式综合服务体。由伯藜基金、哈佛校友投资创建，通过整合业内优秀的财务、法务、人事、IT技术等资源，汇聚中国广大的优秀大学生和专业的退休人员；利用移动互联网和云端技术，通过线下线下的服务方式，优化服务流程，降低服务成本。为中国千万中小企业、创新创业者提供一站超值的服务。核心团队主要由上市公司高管，资深财务、法务、人力资源负责人以及国内知名互联网企业的精英组成。</p>
+      <h3>企业文化</h3>
+      <h6>企业湾的使命</h6>
+      <p>重塑中国企业服务和专业高等教育实践的生态</p>
+      <h6>企业湾的愿景</h6>
+      <p>成为从业人员最多、最具有活力的专业服务机构</p>
+      <h6>企业湾的战略</h6>
+      <p>·聚焦小微企业的需求</p>
+      <p>·借助高校相关资源</p>
+      <p>·弘扬企业家精神</p>
+      <p>·强大的过程能力保证绩效和扩张</p>
+      <h6>企业湾的服务承诺</h6>
+      <p>一站、超值、快速响应</p>
+    </div>
   </div>
 </template>
 
 <style scoped>
+  #about-us {
+    padding-top: 44px;
+    background: #fafafa;
+  }
 
+  #content {
+    padding: 0 10px;
+  }
+
+  h3 {
+    font-size: 18px;
+    line-height: 40px;
+    color: #f29600;
+  }
+
+  h6 {
+    font-size: 15px;
+    line-height: 20px;
+  }
+
+  p {
+    font-size: 13px;
+    line-height: 15px;
+  }
 </style>
--- src/pages/ContactUs.vue
+++ src/pages/ContactUs.vue
@@ -11,11 +11,11 @@
           </tr>
           <tr>
             <td class="label">热 线</td>
-            <td>400-716-8896</td>
+            <td><a href="tel:400-716-8896">400-716-8896</a></td>
           </tr>
           <tr>
             <td class="label">电 话</td>
-            <td>021-54706618</td>
+            <td><a href="tel:021-54706618">021-54706618</a></td>
           </tr>
           <tr>
             <td class="label">邮 箱</td>
@@ -28,77 +28,80 @@
         </table>
       </div>
       <div class="card">
-        <p class="title">企业湾总部</p>
+        <p class="title">南京分公司</p>
         <table>
           <tr>
-            <td class="label">总部地址</td>
-            <td>上海市闵行区剑川路951号零号湾全球创新创业聚集区1号楼南楼508室</td>
-          </tr>
-          <tr>
-            <td class="label">热 线</td>
-            <td>400-716-8896</td>
+            <td class="label">地址</td>
+            <td>南京市玄武区玄武大道699-1号徐庄软件管委会607室</td>
           </tr>
           <tr>
             <td class="label">电 话</td>
-            <td>021-54706618</td>
-          </tr>
-          <tr>
-            <td class="label">邮 箱</td>
-            <td>qiyewan@qiyewan.com</td>
-          </tr>
-          <tr>
-            <td class="label">网 址</td>
-            <td>www.qiyewan.com</td>
+            <td><a href="tel:025-68022609">025-68022609</a></td>
           </tr>
         </table>
       </div>
       <div class="card">
-        <p class="title">企业湾总部</p>
+        <p class="title">镇江分公司</p>
         <table>
           <tr>
-            <td class="label">总部地址</td>
-            <td>上海市闵行区剑川路951号零号湾全球创新创业聚集区1号楼南楼508室</td>
-          </tr>
-          <tr>
-            <td class="label">热 线</td>
-            <td>400-716-8896</td>
+            <td class="label">地址</td>
+            <td>镇江市润州区黄山南路66号中浩国际广场A座1201</td>
           </tr>
           <tr>
             <td class="label">电 话</td>
-            <td>021-54706618</td>
-          </tr>
-          <tr>
-            <td class="label">邮 箱</td>
-            <td>qiyewan@qiyewan.com</td>
-          </tr>
-          <tr>
-            <td class="label">网 址</td>
-            <td>www.qiyewan.com</td>
+            <td><a href="tel:0511-88076713">0511-88076713</a></td>
           </tr>
         </table>
       </div>
       <div class="card">
-        <p class="title">企业湾总部</p>
+        <p class="title">苏州分公司</p>
         <table>
           <tr>
-            <td class="label">总部地址</td>
-            <td>上海市闵行区剑川路951号零号湾全球创新创业聚集区1号楼南楼508室</td>
-          </tr>
-          <tr>
-            <td class="label">热 线</td>
-            <td>400-716-8896</td>
+            <td class="label">地址</td>
+            <td>苏州市姑苏区阊胥路726号南新园3305</td>
           </tr>
           <tr>
             <td class="label">电 话</td>
-            <td>021-54706618</td>
+            <td><a href="tel:17186396121">17186396121</a></td>
+          </tr>
+        </table>
+      </div>
+      <div class="card">
+        <p class="title">北京分公司</p>
+        <table>
+          <tr>
+            <td class="label">地址</td>
+            <td>北京市海淀区中关村创业大街六号楼4层企业湾</td>
           </tr>
           <tr>
-            <td class="label">邮 箱</td>
-            <td>qiyewan@qiyewan.com</td>
+            <td class="label">电 话</td>
+            <td><a href="tel:15712846498">15712846498</a></td>
+          </tr>
+        </table>
+      </div>
+      <div class="card">
+        <p class="title">深圳分公司</p>
+        <table>
+          <tr>
+            <td class="label">地址</td>
+            <td>深圳市宝安区新湖路华侨新村西提一巷金华大厦12层1216</td>
           </tr>
           <tr>
-            <td class="label">网 址</td>
-            <td>www.qiyewan.com</td>
+            <td class="label">电 话</td>
+            <td><a href="tel:0755-36301780">0755-36301780</a></td>
+          </tr>
+        </table>
+      </div>
+      <div class="card">
+        <p class="title">成都分公司</p>
+        <table>
+          <tr>
+            <td class="label">地址</td>
+            <td>成都市高新益州大道1800号移动互联创业大厦G1，201</td>
+          </tr>
+          <tr>
+            <td class="label">电 话</td>
+            <td><a href="tel:13518211800">13518211800</a></td>
           </tr>
         </table>
       </div>
